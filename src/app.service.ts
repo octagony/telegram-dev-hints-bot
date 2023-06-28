0feb72ed37cf95b08c229fc7b62426ca2cd27671
octagony <octagony@pop-os.localdomain>
2023-06-28 19:50:49 +0300

    [FEAT] Added more git information

--- src/app.service.ts
+++ src/app.service.ts
@@ -11,6 +11,18 @@ export class AppService {
 
 <b>⚪ git config --global user.email “you@example.com”</b>
       Set the e-mail address that will be attached to your commits and tags.
+
+<b>⚪ git config --global color.ui auto </b> 
+      Enable some colorization of Git output.
+
+<b>⚪ git init [project name]</b>
+      Create a new local repository. If [project name] is provided, Git will create a new directory name [project name] and will initialize a repository inside it. If [project name] is not provided, then a new repository is initialized in the current directory.
+
+<b>⚪ git clone [project url]</b>
+      Downloads a project with the entire history from the remote repository.
+
+<b>⚪ git status</b>
+      Displays the status of your working directory. Options include new, staged, and modified files. It will retrieve branch name, current commit identifier, and changes pending commit.
     `);
   }
 }
